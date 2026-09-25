304c334862bb70b8451a9c1e9899c0167a4dbc60
Azuko8 <andreimncalex@posteo.com>
2026-09-25 18:27:32 +0300

    add libreoffice

--- profiles/archgnome.sh
+++ profiles/archgnome.sh
@@ -38,7 +38,7 @@ fi
 echo "Installing pacman packages..."
 echo ""
 
-if sudo pacman -S --needed --noconfirm fish git micro obs-studio btrfs-assistant solaar timeshift librewolf ghostty flatpak > /dev/null; then
+if sudo pacman -S --needed --noconfirm fish git micro obs-studio btrfs-assistant solaar timeshift librewolf ghostty flatpak libreoffice > /dev/null; then
     echo "Packages installed successfully."
 else
     echo "Error: Installation failed."
