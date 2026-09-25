dbffcb23667c4052980963419fcf54fe8f65b62b
Azuko8 <andreimncalex@posteo.com>
2026-09-25 18:26:26 +0300

    add libreoffice

--- profiles/archcosmic.sh
+++ profiles/archcosmic.sh
@@ -35,7 +35,7 @@ fi
 echo "Installing pacman packages..."
 echo ""
 
-if sudo pacman -S --needed --noconfirm fish git micro obs-studio btrfs-assistant solaar timeshift librewolf kitty flatpak > /dev/null; then
+if sudo pacman -S --needed --noconfirm fish git micro obs-studio btrfs-assistant solaar timeshift librewolf kitty flatpak libreoffice > /dev/null; then
     echo "Packages installed successfully."
 else
     echo "Error: Installation failed."
